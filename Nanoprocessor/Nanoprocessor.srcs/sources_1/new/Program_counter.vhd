775c65c39e339301d0b2b9ac23d5fd0e620c1dde
VilashNaveen <vilashnaveen@gmail.com>
2023-05-25 14:24:32 +0530

    rom updated

---
+++ Nanoprocessor/Nanoprocessor.srcs/sources_1/new/Program_counter.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 25.05.2023 14:24:19
+-- Design Name: 
+-- Module Name: Program_counter - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Program_counter is
+--  Port ( );
+end Program_counter;
+
+architecture Behavioral of Program_counter is
+
+begin
+
+
+end Behavioral;
